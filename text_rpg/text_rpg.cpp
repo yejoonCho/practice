86ba2f938bae83594220a101bdd306baca26bfb8
yejoon <actyejoon@gmail.com>
2021-12-28 23:01:03 +0900

    text_rpg

---
+++ text_rpg/text_rpg.cpp
@@ -0,0 +1,199 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+enum PlayerType
+{
+	Knight =1,
+	Archer = 2,
+	Mage =3,
+};
+
+enum MonsterType
+{
+	Slime = 1,
+	Orc = 2,
+	Skeleton = 3,
+};
+
+struct StatInfo
+{
+	int hp = 0;
+	int attack = 0;
+	int defence = 0;
+};
+
+void EnterLobby();
+void PrintMessage(const char* msg);
+void CreatePlayer(StatInfo* playerInfo);
+void PrintStatInfo(const char* name, const StatInfo& info);
+void EnterGame(StatInfo* playerInfo);
+void CreateMonsters(StatInfo monsterInfo[], int count);
+bool EnterBattle(StatInfo* playerInfo, StatInfo* monsterInfo);
+
+int main()
+{
+	srand((unsigned) time(nullptr));
+	EnterLobby();
+	return 0;
+}
+
+void EnterLobby()
+{
+	while (true)
+	{
+		PrintMessage("로비에 입장했습니다.");
+
+		StatInfo playerInfo;
+		CreatePlayer(&playerInfo);
+		PrintStatInfo("Player", playerInfo);
+
+		EnterGame(&playerInfo);
+	}
+}
+
+void PrintMessage(const char* msg)
+{
+	cout << "********************" << endl;
+	cout << msg << endl;
+	cout << "********************" << endl;
+}
+
+void CreatePlayer(StatInfo* playerInfo)
+{
+	bool isSelected = false;
+	while (!isSelected)
+	{
+		PrintMessage("캐릭터 생성창");
+		PrintMessage("[1] 기사  [2] 궁수  [3] 법사");
+		cout << ">" << endl;
+
+		int input;
+		cin >> input;
+
+		switch (input)
+		{
+		case Knight:
+			playerInfo->hp = 100;
+			playerInfo->attack = 10;
+			playerInfo->defence = 5;
+			isSelected = true;
+			break;
+		case Archer:
+			playerInfo->hp = 80;
+			playerInfo->attack = 15;
+			playerInfo->defence = 3;
+			isSelected = true;
+			break;
+		case Mage:
+			playerInfo->hp = 50;
+			playerInfo->attack = 25;
+			playerInfo->defence = 1;
+			isSelected = true;
+			break;
+		}
+	}
+}
+
+void PrintStatInfo(const char* name, const StatInfo& info) 
+{
+	cout << "*****************************" << endl;
+	cout << name << " : HP=" << info.hp << " ATT=" << info.attack << " DEF=" << info.defence << endl;
+	cout << "*****************************" << endl;
+}
+
+void EnterGame(StatInfo* playerInfo)
+{
+	const int MONSTER_COUNT = 2;
+	PrintMessage("게임에 입장했습니다.");
+	
+	while (true)
+	{
+		StatInfo monsterInfo[MONSTER_COUNT];
+		CreateMonsters(monsterInfo, MONSTER_COUNT);
+
+		for (int i = 0; i < MONSTER_COUNT; i++)
+			PrintStatInfo("Monster", monsterInfo[i]);
+
+		PrintStatInfo("Player", *playerInfo);
+
+		PrintMessage("[1] 전투  [2] 전투  [3] 도망");
+
+		int input;
+		cin >> input;
+
+		if (input == 1 || input == 2)
+		{
+			int index = input - 1;
+			bool victory = EnterBattle(playerInfo, &monsterInfo[index]);
+			if (victory == false)
+				break;
+
+		}
+	}
+}
+
+void CreateMonsters(StatInfo monsterInfo[], int count)
+{
+	for (int i = 0; i < count; i++)
+	{
+		int randValue = 1 + rand() % 3;
+		switch (randValue)
+		{
+		case Slime:
+			monsterInfo[i].hp = 30;
+			monsterInfo[i].attack = 5;
+			monsterInfo[i].defence = 1;
+			break;
+		case Orc:
+			monsterInfo[i].hp = 40;
+			monsterInfo[i].attack = 8;
+			monsterInfo[i].defence = 2;
+			break;
+		case Skeleton:
+			monsterInfo[i].hp = 50;
+			monsterInfo[i].attack = 15;
+			monsterInfo[i].defence = 3;
+			break;
+		}
+	}
+}
+
+bool EnterBattle(StatInfo* playerInfo, StatInfo* monsterInfo)
+{
+	while (true)
+	{
+		int damage = playerInfo->attack - monsterInfo->defence;
+		if (damage < 0)
+			damage = 0;
+
+		monsterInfo->hp -= damage;
+		if (monsterInfo->hp < 0)
+			monsterInfo->hp = 0;
+
+		PrintStatInfo("Monster", *monsterInfo);
+
+		if (monsterInfo->hp == 0)
+		{
+			PrintMessage("몬스터를 처치했습니다.");
+			return true;
+		}
+		
+		damage = monsterInfo->attack - playerInfo->defence;
+		if (damage < 0)
+			damage = 0;
+
+		playerInfo->hp -= damage;
+		if (playerInfo->hp < 0)
+			playerInfo->hp = 0;
+
+		PrintStatInfo("Player", *playerInfo);
+
+		if (playerInfo->hp == 0)
+		{
+			PrintMessage("Game Over");
+			return false;
+		}
+	}
+}
